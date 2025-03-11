a6a6bd3ee5463bd131938fc1e7cd4d44ba2ba4ef
parvez <parvezreza112@gmail.com>
2025-03-12 02:11:25 +0600

    added toggle js file and mobile responsive

---
+++ script/toggle1.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("change-location")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "blog.html";
+  });
---
+++ script/toggle2.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("change-location1")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "index.html";
+  });
